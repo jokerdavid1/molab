d146c8bcf16130570c081ba23ed484c5555e6176
jokerdavid1 <60370168+jokerdavid1@users.noreply.github.com>
2026-04-13 14:04:10 -0600

    Update server.ts

--- app/utils/supabase/server.ts
+++ app/utils/supabase/server.ts
@@ -1,6 +1,19 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
+type CookieToSet = {
+  name: string
+  value: string
+  options?: {
+    path?: string
+    domain?: string
+    maxAge?: number
+    httpOnly?: boolean
+    secure?: boolean
+    sameSite?: 'lax' | 'strict' | 'none'
+  }
+}
+
 export async function createClient() {
   const cookieStore = await cookies()
 
@@ -12,7 +25,7 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: CookieToSet[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
